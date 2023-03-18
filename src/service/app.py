e3de58916f4b787af2607d3f67f54f88bb2c74fe
JongHoB <78012131+JongHoB@users.noreply.github.com>
2023-03-18 21:38:37 +0900

    [ADD] email 기능 추가/변동 필요

--- src/service/app.py
+++ src/service/app.py
@@ -1,17 +1,50 @@
+import time
+from selenium.webdriver.common.by import By
 import sys  # acquire parameters
 import pandas as pd
 import os
 
-# 코드 생략...
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 
-def op():
-    os.chdir(os.getcwd())
-    df1 = pd.read_table(sys.argv[1], encoding='utf-8')
-    print(df1, end='')
-    sys.stdout.flush()
+def find_btn_menu_1():
+    try:
+        btn_menu_1 = driver.find_element(
+            By.XPATH, "//a[contains(text(), '학생생활신청')]")
+        time.sleep(1)
+        btn_menu_1.click()
+    except:
+        print("Error: <a> 태그 중에서 텍스트가 '학생생활신청'인 요소가 없음")
+    return
+
+
+def find_btn_menu_2():
+    try:
+        btn_menu_2 = driver.find_element(
+            By.XPATH, "//a[contains(text(), '소학회상세정보신청')]")
+        time.sleep(1)
+        btn_menu_2.click()
+    except:
+        print("Error: <a> 태그 중에서 텍스트가 '소학회상세정보신청'인 요소가 없음")
+    return
 
 
 # import sys를 활용하여 매개변수를 받아온다.
 if __name__ == '__main__':
-    op()
+    driver.get('https://mhaksa.ajou.ac.kr:30443/index.html')
+    driver.maximize_window()
+    input_id = driver.find_element('name', 'userId')
+    input_pw = driver.find_element('name', 'password')
+    btn_login = driver.find_element('id', 'loginSubmit')
+    input_id.send_keys('nhle0217')
+    input_pw.send_keys('030217nana')
+    btn_login.click()
+
+    find_btn_menu_1()
+
+    find_btn_menu_2()
+
+    while (True):
+        pass
---
+++ src/service/sendEmail.py
@@ -0,0 +1,27 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+def mail(content):
+    send_email = "qorwhdghjr@ajou.ac.kr"
+    send_pw = "100jongho^^"
+    results = []
+
+    s = smtplib.SMTP_SSL('smtp.gmail.com')
+    s.login(send_email, send_pw)
+    receive_emails = ['qorwhdgh@gmail.com']
+    for receive in receive_emails:
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = "TEST"
+        msg['From'] = send_email
+        msg['To'] = receive
+        part1 = MIMEText(content, 'plain')
+        msg.attach(part1)
+        s.sendmail(send_email, receive, msg.as_string())
+        s.quit()
+
+
+if __name__ == '__main__':
+    content = input()
+    mail(content)
